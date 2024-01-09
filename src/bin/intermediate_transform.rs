e1beb66736580cdb195eb29645c1ec3f94f72ed2
Pascal Berrang <git@paberr.net>
2024-01-09 18:34:31 +0000

    Fix intermediate transform to cut down powers correctly

--- src/bin/intermediate_transform.rs
+++ src/bin/intermediate_transform.rs
@@ -4,7 +4,9 @@ use ark_bw6_761::BW6_761;
 use ark_ec::pairing::Pairing;
 use ark_mnt4_753::MNT4_753;
 use ark_mnt6_753::MNT6_753;
+use ark_serialize::CanonicalDeserialize;
 use gumdrop::Options;
+use phase2::load_circuit::Matrices;
 use snark_setup_operator::data_structs::Setup;
 use snark_setup_operator::setup_filename;
 use snark_setup_operator::transcript_data_structs::Transcript;
@@ -16,15 +18,25 @@ use snark_setup_operator::{
 use std::ops::Neg;
 use std::{fs::File, io::Read};
 
+/// If a circuit filename is given, the phase2 powers are estimated from the circuit itself.
+/// Otherwise, the powers must be given by the additional argument.
 #[derive(Debug, Options, Clone)]
 pub struct IntermediateTransformOpts {
     help: bool,
     #[options(help = "the path of the transcript json file", default = "transcript")]
     pub transcript_path: String,
+    #[options(help = "setup id", required)]
+    pub setup_id: usize,
+    #[options(help = "powers in phase2")]
+    pub phase2_powers: Option<usize>,
+    #[options(help = "circuit filename")]
+    pub circuit_filename: Option<String>,
 }
 
 pub struct IntermediateTransform {
     pub transcript: Transcript,
+    pub setup_id: usize,
+    pub phase2_powers: usize,
 }
 
 impl IntermediateTransform {
@@ -36,7 +48,50 @@ impl IntermediateTransform {
             .expect("Should have read transcript file.");
         let transcript: Transcript = serde_json::from_str::<Transcript>(&transcript)?;
 
-        Ok(Self { transcript })
+        let phase2_powers = if let Some(ref circuit_filename) = opts.circuit_filename {
+            let ceremony = transcript
+                .rounds
+                .iter()
+                .last()
+                .expect("Round not found in transcript");
+
+            let setup = &ceremony.setups[opts.setup_id];
+            match setup.parameters.curve_kind.as_str() {
+                "bw6" => Self::estimate_circuit_powers::<BW6_761>(circuit_filename),
+                "bls12_377" => Self::estimate_circuit_powers::<Bls12_377>(circuit_filename),
+                "mnt4_753" => Self::estimate_circuit_powers::<MNT4_753>(circuit_filename),
+                "mnt6_753" => Self::estimate_circuit_powers::<MNT6_753>(circuit_filename),
+                _ => {
+                    return Err(VerifyTranscriptError::UnsupportedCurveKindError(
+                        setup.parameters.curve_kind.clone(),
+                    )
+                    .into())
+                }
+            }
+        } else {
+            opts.phase2_powers
+                .expect("Need to give either phase2_powers or circuit_filename")
+        };
+
+        Ok(Self {
+            transcript,
+            setup_id: opts.setup_id,
+            phase2_powers,
+        })
+    }
+
+    fn estimate_circuit_powers<P: Pairing>(circuit_filename: &str) -> usize {
+        let mut file = File::open(circuit_filename).unwrap();
+        let mut buffer = Vec::<u8>::new();
+        file.read_to_end(&mut buffer).unwrap();
+        let m = Matrices::<P>::deserialize_compressed(&*buffer).unwrap();
+
+        std::cmp::max(
+            m.num_constraints,
+            m.num_witness_variables + m.num_instance_variables,
+        )
+        .next_power_of_two()
+        .trailing_zeros() as usize
     }
 
     fn run(&self) -> Result<()> {
@@ -47,18 +102,17 @@ impl IntermediateTransform {
             .last()
             .expect("Round not found in transcript");
 
-        for setup in ceremony.setups.iter() {
-            match setup.parameters.curve_kind.as_str() {
-                "bw6" => self.transform::<BW6_761>(setup),
-                "bls12_377" => self.transform::<Bls12_377>(setup),
-                "mnt4_753" => self.transform::<MNT4_753>(setup),
-                "mnt6_753" => self.transform::<MNT6_753>(setup),
-                _ => Err(VerifyTranscriptError::UnsupportedCurveKindError(
-                    setup.parameters.curve_kind.clone(),
-                )
-                .into()),
-            }?;
-        }
+        let setup = &ceremony.setups[self.setup_id];
+        match setup.parameters.curve_kind.as_str() {
+            "bw6" => self.transform::<BW6_761>(setup),
+            "bls12_377" => self.transform::<Bls12_377>(setup),
+            "mnt4_753" => self.transform::<MNT4_753>(setup),
+            "mnt6_753" => self.transform::<MNT6_753>(setup),
+            _ => Err(VerifyTranscriptError::UnsupportedCurveKindError(
+                setup.parameters.curve_kind.clone(),
+            )
+            .into()),
+        }?;
 
         Ok(())
     }
@@ -73,7 +127,7 @@ impl IntermediateTransform {
         phase1_cli::prepare_phase2(
             setup_filename!(PHASE2_INIT_FILENAME, setup.setup_id),
             setup_filename!(COMBINED_FILENAME, setup.setup_id),
-            setup.parameters.power,
+            self.phase2_powers,
             &parameters,
         )?;
 
